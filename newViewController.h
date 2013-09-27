631ee622429aa2ee065ff3751d3b2360aab3aee3
poojamurthyK <pooja.murthy79@gmail.com>
2013-09-27 19:38:14 +0530

    Create newViewController.h

---
+++ newViewController.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface newViewController : UIViewController
+{
+  //ing *temp;
+}
+@property (weak, nonatomic) IBOutlet UILabel *userOutput;
+- (IBAction)logout:(UIButton *)sender;
+@property (retain, nonatomic) NSString * temp;
+@end
